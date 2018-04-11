9a23fe62077974a8bd47243be7977f5dd1c8d114
joychen <joychen1579@163.com>
2018-04-11 17:15:44 +0800

    java基本数据类型
    java基本数据类型

---
+++ 02BaseType/src/BaseType.java
@@ -0,0 +1,63 @@
+/**
+	* @ClassName: BaseType
+	* @Description: TODO(这里用一句话描述这个类的作用)
+	* @author joychen
+	* @date 2018年3月14日
+	* 江西微软件有限公司
+	*/
+public class BaseType {
+	
+	public static final double PI = 3.14159265357; 
+
+	public static void main(String[] args) {
+//		//整形
+//		int  num1 = 4;   //4 个字节
+//		short num2 = 5;   //2个字节     
+//		long num3 = 3333;   //8个字节
+//		byte b = 22;    //1字节    -128  ~  127
+//		
+//		
+//		//浮点形
+//		float num4 = 4.4f;   //单精度   4个字节
+//		double  num5  = 55.5555;    //双精度  8个字节
+//		
+//		
+//		//字符形
+//		char  c = 'a';    //2个字节
+//		
+//		boolean isSuceess = true;  //  true  false 
+//		
+//		System.out.println(num1+num2+"a");
+//		int weight = 067;  //8进制  表示55
+//		int weight = 0X37;   //16进制表示  55
+//		int weight = 0b110111;   //2进制来表示 55
+//		int weight = 55;
+//		System.out.println("体重是"+weight+"kg");
+//		weight = 60;
+//		System.out.println("一个月后体重是"+weight+"kg");
+//		
+//		System.out.println("PI=" + PI);
+//		String des = "my name is joychen";
+//		String []sp = des.split(" ");
+//		System.out.println(sp.length);
+//		
+//		
+//		int ab = 44;
+//		long cd = ab;
+//		System.out.println(cd);
+//		
+//		long num1 = 6666666;      
+//		System.out.println(num1);
+//		short num2 = (short) num1;
+//		System.out.println(num2);
+		
+//		double num3 = 66.666;
+//		float num4 = (float) num3;
+		
+//		double num5 = 777.77;    //小数转整数会直接把小数切掉
+//		int num6 = (int) num5;
+//		System.out.println(num6);
+		
+	}
+	
+}
